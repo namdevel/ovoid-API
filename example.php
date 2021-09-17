d82dba4298a79a25e87a4b65a31e12be67842cdc
几句レし <62225978+namdevel@users.noreply.github.com>
2021-09-17 09:59:55 +0700

    Update example.php

--- example.php
+++ example.php
@@ -1,6 +1,26 @@
-<?php 
-require_once __DIR__ . '/vendor/autoload.php';
+<?php
+require('src/Ovo.php');
 
 use Namdevel\Ovo;
-$ovo = new Ovo();
-echo $ovo->login2FA('<phone number>');
+
+$token = "eyJhbGciOiJSUzI1NiJ9.eyJleHBpc...."; // your ovo auth token
+$app = new Ovo($token);
+
+echo $app->getLastTransactions(); // get 5 last transaction
+echo $app->getFavoriteTransfer(); // get favorite transfer
+echo $app->getEmail(); // get account email
+echo $app->transactionHistory(); // get transaction history
+echo $app->walletInquiry(); // wallet inquiry
+echo $app->getOvoCash(); // get ovo balance
+echo $app->getOvoCashCardNumber(); // get ovo cash card number
+echo $app->getOvoPointsCardNumber(); // get ovo point card number
+echo $app->getOvoPoints(); // get ovo point balance
+echo $app->getPointDetails(); // get ovo point history
+echo $app->getBillerList(); // get biller list
+echo $app->getBillerCategory('<category_id>'); // get biller category
+echo $app->getDenominations('<product_id>'); // get denomination
+echo $app->getBankList(); // get bank list
+echo $app->getUnreadNotifications(); // get total unread notification
+echo $app->getAllNotifications(); // get all notifications
+echo $app->getInvestment(); // get investment data
+echo $app->isOVO('<amount>', '<phone_number>'); // ovo account validate
